5fc6e1b79702dc93601453b770fb9ea6585bd402
DomiTheKava <Dominikpenkava@yahoo.com>
2024-10-24 13:46:41 -0500

    V1

--- SaveMyDefaultsPersistance/ViewController.swift
+++ SaveMyDefaultsPersistance/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var uiImageView: UIImageView!
+    
+    @IBOutlet weak var sliderBar: UISlider!
+    
+    @IBOutlet weak var switchButton: UISwitch!
+    
+    @IBOutlet weak var serialNumberEntry: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func btnSaveClicked(_ sender: Any) {
+        UserDefaults.standard.set(serialNumberEntry.text, forKey: "Text")
+        UserDefaults.standard.set(switchButton.isOn, forKey: "Switch")
+        UserDefaults.standard.set(sliderBar.value, forKey: "Slider")
+    }
+    
+    @IBAction func btnLoadClicked(_ sender: Any) {
+        switchButton.isOn = UserDefaults.standard.bool(forKey: "Switch")
+        sliderBar.value = UserDefaults.standard.float(forKey: "Slider")
+        serialNumberEntry.text = UserDefaults.standard.string(forKey: "Text")
+    }
+    
+    @IBAction func btnDeleteClicked(_ sender: Any) {
+        sliderBar.value = 0.5
+        serialNumberEntry.text = ""
+        switchButton.isOn = true
+    }
 }
 
